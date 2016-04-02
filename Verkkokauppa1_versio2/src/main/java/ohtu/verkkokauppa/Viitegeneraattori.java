eb03a097176575db4e60ef211a3abb26a9e5b5c3
sallav <salla_mari@hotmail.com>
2016-04-02 17:41:57 +0300

    verkkokauppa

---
+++ Verkkokauppa1_versio2/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -0,0 +1,18 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Viitegeneraattori implements Generaattori {
+    
+    private int seuraava;
+    
+    public Viitegeneraattori(){
+        seuraava = 1;    
+    }
+    
+    @Override
+    public int uusi(){
+        return seuraava++;
+    }
+}
